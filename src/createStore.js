0c9dc56f8a2c9bcca7e51748aa905ce9467a5fa0
colesayer <colesayershapiro@gmail.com>
2017-11-17 16:49:39 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,6 +1,25 @@
 export default function createStore(reducer) {
-  // add your code here
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+
+  dispatch({type: '@@init'})
+
+  function getState(){
+    return state
+  }
+
+  return {
+    dispatch,
+    getState
+  }
+
+
 }
+
 
 function render() {
   const container = document.getElementById('container');
